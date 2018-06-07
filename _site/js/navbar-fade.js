16b77cf991f5f2b7105690d82a002dd6a68bd033
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2018-06-07 19:15:57 +0200

    navigation bars added

--- _site/js/navbar-fade.js
+++ _site/js/navbar-fade.js
@@ -1,7 +1,22 @@
-var $nav = $("#navigation");
-if(scrolltop >= $nav.offset().top + $nav.height()) {
-  $('#fixedbar').fadeIn(250);
-}
-else {
-  $('#fixedbar').fadeOut(250);
-}
+
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $("#menu").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 300) {
+                $('#menu').fadeIn();
+            } else {
+                $('#menu').fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
