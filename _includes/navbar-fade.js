d9322a2c1f900e86e647998776acc02800bc026e
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2018-06-07 19:22:26 +0200

    js navbar

--- _includes/navbar-fade.js
+++ _includes/navbar-fade.js
@@ -9,7 +9,7 @@
         $(window).scroll(function () {
 
                  // set distance user needs to scroll before we start fadeIn
-            if ($(this).scrollTop() > 300) {
+            if ($(this).scrollTop() > 500) {
                 $('#menu').fadeIn();
             } else {
                 $('#menu').fadeOut();
